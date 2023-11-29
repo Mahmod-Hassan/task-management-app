71fa569a45d799d3142a22c357782e58b3f75806
Mahmod Hassan <mahmodhasan7788@gmail.com>
2023-11-29 11:57:31 +0600

    retrive data from local storage and show ui in a tabular format

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,27 @@
 import Button from "./Button";
 
 const TaskItem = ({ task }) => {
-  const { name, description, priority } = task;
+  const { id, name, description, priority, completed } = task;
+  const tdClass = "py-2 px-4 border-r";
   return (
-    <div className="container flex justify-between items-center mx-auto border-b border-2">
-      <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-500" />{" "}
-      |<h2>{name}</h2> |<p>{description}</p> |
-      <p className="py-2 px-4">{priority}</p>
-      <Button classes="bg-blue-500 ">Edit</Button>
-      <Button classes="bg-red-500">Delete</Button>
-    </div>
+    <tr className={`border-t ${completed ? "line-through" : "none"}`}>
+      <td className={`${tdClass}`}>
+        <input
+          type="checkbox"
+          className="form-checkbox h-5 w-5 text-blue-500"
+          onClick={() => toggoleComplete(id)}
+        />
+      </td>
+      <td className={`${tdClass}`}>{name}</td>
+      <td className={`${tdClass}`}>{description}</td>
+      <td className={`${tdClass}`}>{priority}</td>
+      <td className={`${tdClass}`}>
+        <Button classes="bg-blue-500">Edit</Button>
+      </td>
+      <td className={`${tdClass}`}>
+        <Button classes="bg-red-500">Delete</Button>
+      </td>
+    </tr>
   );
 };
 
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,12 +1,27 @@
 import TaskItem from "../components/TaskItem";
 
 const TaskList = ({ tasks }) => {
+  const tdClass = "py-2 px-4 border-r";
   return (
-    <ul>
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
-    </ul>
+    <div className="container mx-auto p-4">
+      <table className="min-w-full bg-white border border-gray-300">
+        <thead>
+          <tr className="bg-gray-300">
+            <th className={`${tdClass}`}>Toggole</th>
+            <th className={`${tdClass}`}>Task</th>
+            <th className={`${tdClass}`}>Description</th>
+            <th className={`${tdClass}`}>Priority</th>
+            <th className={`${tdClass}`}>Edit</th>
+            <th className={`${tdClass}`}>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
