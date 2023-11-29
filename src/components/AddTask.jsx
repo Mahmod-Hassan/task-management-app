b0da11851c65030148e32e59e50d0c43249e981f
Mahmod Hassan <mahmodhasan7788@gmail.com>
2023-11-29 07:15:32 +0600

    inititalize react with vite

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,14 @@
+const AddTask = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Task name</label>
+          <input type="text" placeholder="I will do..." />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
